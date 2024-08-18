e5575cd1b7cf98ccba51bfc718900fbb33e43fa4
Shahed <shahedissa9@gmail.com>
2024-08-17 23:53:48 -0600

    sidebar complete

--- planify/src/components/Calendar/CalendarHeader.js
+++ planify/src/components/Calendar/CalendarHeader.js
@@ -21,26 +21,26 @@ export default function CalendarHeader() {
           : dayjs().month()
       );
     }
-    
+
     return (
       <header className="px-4 py-2 flex items-center">
         <img src={logo} alt="calendar" className="mr-2 w-8 h-8" />
-        <h1 className="mr-10 text-l text-pink-500 font-bold">
+        <h1 className="mr-10 text-l text-pink-400 font-bold">
           Calendar
         </h1>
         <button onClick={handleReset} className="border rounded py-2 px-4 mr-5 text-sm">Today</button>
         <button onClick={handlePrevMonth}>
-        <span className="material-icons-outlined cursor-pointer text-pink-500 mx-2">chevron_left</span>
+        <span className="material-icons-outlined cursor-pointer text-pink-400 mx-2">chevron_left</span>
         </button>
 
-        <h2 className="mx-4 text-l text-pink-500 font-bold">
+        <h2 className="mx-4 text-l text-pink-400 font-bold">
           {dayjs(new Date(dayjs().year(), monthIndex)).format(
             "MMMM YYYY"
           )}
         </h2>
 
         <button onClick={handleNextMonth}>
-          <span className="material-icons-outlined cursor-pointer text-pink-500 mx-2">chevron_right</span>
+          <span className="material-icons-outlined cursor-pointer text-pink-400 mx-2">chevron_right</span>
         </button>
         
       </header>
