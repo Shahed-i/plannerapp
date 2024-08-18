2b68dffb4678ce5325844eb5a43863b73d5e16e9
Shahed <shahedissa9@gmail.com>
2024-08-17 22:40:40 -0600

    created calendar layout

--- planify/src/pages/Calendar.js
+++ planify/src/pages/Calendar.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import NavBar from '../components/NavBar';
+import { getMonth } from '../util';
+import CalendarHeader from '../components/Calendar/CalendarHeader';
+import Sidebar from '../components/Calendar/Sidebar';   
+import Month from '../components/Calendar/Month';
 
 function Calendar() {
+    const [currentMonth, setCurrentMonth] = useState(getMonth())
+    
     return (
         <div>
         <NavBar />
-        <h1>
-            This is Calendar page!
-        </h1>
+        
+        <React.Fragment>
+            <div className="h-screen flex flex-colums">
+                <CalendarHeader />
+                <div className="flex flex-1"> 
+                    <Sidebar />
+                    <Month month={currentMonth}/>
+                </div>
+            </div>
+        </React.Fragment>
+
         </div>
     );
     };
