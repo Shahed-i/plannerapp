79b1bcbab69c0d6face6f17ecc2b24e02b33026d
Shahed <shahedissa9@gmail.com>
2024-08-20 18:23:34 -0600

    added clear labels button

--- planify/src/components/Calendar/Labels.js
+++ planify/src/components/Calendar/Labels.js
@@ -1,24 +1,50 @@
 import React, { useContext } from "react";
 import GlobalContext from "../../context/GlobalContext";
 
+const labelClassesMap = {
+  Purple: "bg-purple-300",
+  Orange: "bg-orange-300",
+  Blue: "bg-blue-300",
+  Green: "bg-green-300",
+  Pink: "bg-pink-300",
+  Yellow: "bg-yellow-300",
+};
+
 export default function Labels() {
-  const { labels, updateLabel } = useContext(GlobalContext);
+  const { labels, updateLabel, clearAllLabels } = useContext(GlobalContext);
+
   return (
     <React.Fragment>
-      <p className="text-gray-500 font-bold mt-10">Label</p>
-      {labels.map(({ label: lbl, checked }, idx) => (
-        <label key={idx} className="items-center mt-3 block">
-          <input
-            type="checkbox"
-            checked={checked}
-            onChange={() =>
-              updateLabel({ label: lbl, checked: !checked })
-            }
-            className={`form-checkbox h-5 w-5 text-${lbl}-400 rounded focus:ring-0 cursor-pointer`}
-          />
-          <span className="ml-2 text-gray-700 capitalize">{lbl}</span>
-        </label>
-      ))}
+      <p className="text-pink-500 font-bold mt-10">Labels</p>
+      {labels.map(({ label: lbl, checked }, idx) => {
+        const colorClass = labelClassesMap[lbl] || 'bg-gray-300'; // Default color if label is not in the map
+
+        return (
+          <label key={idx} className="items-center mt-3 block">
+            <input
+              type="checkbox"
+              checked={checked}
+              onChange={() =>
+                updateLabel({ label: lbl, checked: !checked })
+              }
+              className={`form-checkbox h-5 w-5 rounded focus:ring-0 cursor-pointer`}
+              style={{
+                accentColor: colorClass, // checkmark color
+                backgroundColor: checked ? colorClass : 'transparent', // Checkbox background color when checked
+                borderColor: checked ? colorClass : 'gray', // Border color when checked
+              }}
+            />
+            <span className={`ml-2 capitalize ${colorClass.replace('bg-', 'text-')}`}>{lbl}</span> 
+          </label>
+        );
+      })}
+
+      <button
+        className="mt-5 bg-pink-500 text-white py-1 px-3 rounded hover:bg-red-500"
+        onClick={clearAllLabels}
+      >
+        Clear All Labels
+      </button>
     </React.Fragment>
   );
 }
